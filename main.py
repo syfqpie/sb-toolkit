6a7fdf02946fc2fcb1f346fdb00e15a747a4d247
Syafiq Basri <syafiq@talentx.ai>
2022-09-12 22:48:52 +0800

    Added all option

--- main.py
+++ main.py
@@ -12,22 +12,28 @@ app = typer.Typer()
 
 
 @app.command(no_args_is_help=True)
-def pdf_split(file_name: Path = typer.Argument(..., help='PDF file name', metavar='🍄 file name')):
+def pdf_split(file_name: Path = typer.Argument(..., help='PDF file name', metavar='🍄 file name'),
+    all: bool = typer.Option(False, help='All pages', metavar='🍄 all pages')):
     '''
     Split PDF into files
     '''
     # Check file existance
     Helper.file_checker(file_name)
+    start = None
+    end = None
+    step = None
 
-    start = IntPrompt.ask('➡️ Start page')
-    end = IntPrompt.ask('➡️ End page')
-    step = IntPrompt.ask('➡️ Step')
+    # Prompt to get info
+    if not all:
+        start = IntPrompt.ask('➡️ Start page')
+        end = IntPrompt.ask('➡️ End page')
+        step = IntPrompt.ask('➡️ Step')
 
-    # Validate if start > end
-    if start and end and start > end:
-        raise typer.BadParameter(
-            f'⚠️ End page should be higher than start page'
-        )
+        # Validate if start > end
+        if start and end and start > end:
+            raise typer.BadParameter(
+                f'⚠️ End page should be higher than start page'
+            )
 
     # Get information
     PDFKit.extract_info(file_name)
